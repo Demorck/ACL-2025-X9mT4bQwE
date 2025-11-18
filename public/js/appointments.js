f729ff788127ec77b2cbbd97a088470a14f0146d
Demorck <57636452+Demorck@users.noreply.github.com>
2025-11-18 21:52:45 +0100

    Fix le merge des changements d'Anna (y a encore quelques bugs mais chill)

--- public/js/appointments.js
+++ public/js/appointments.js
@@ -4,6 +4,18 @@ document.addEventListener("change", function (e) {
         if (!contenuCache) return;
 
         contenuCache.style.display = e.target.checked ? "block" : "none";
+    } else if (e.target.id === "date_debut") {
+        updateDateFin();
+    } else if (e.target.id === "heure_debut") {
+        updateHeureFin();
+    }
+});
+
+document.addEventListener("blur", function (e) {
+    if (e.target.id === "date_fin") {
+        validiteDateFin();
+    } else if (e.target.id === "heure_fin") {
+        updateHeureFin();
     }
 });
 
@@ -12,6 +24,48 @@ document.addEventListener("change", function (e) {
         afficherCalendar();
     }
 });
+
+//permet de vérifier si la date de fin est antérieur à la date de debut
+function validiteDateFin(){
+    let dateDebAppointment = document.getElementById('date_debut');
+    let dateFinAppointment = document.getElementById('date_fin');
+
+    const valueDateDeb = dateDebAppointment.value;
+    const valueDateFin = dateFinAppointment.value;
+
+    if(valueDateFin && valueDateDeb && valueDateFin < valueDateDeb){
+        dateFinAppointment.value = valueDateDeb;
+    }
+}
+
+//permet de changer la date de fin en fonction de la date de debut
+function updateDateFin(){
+    let dateDebAppointment = document.getElementById('date_debut');
+    let dateFinAppointment = document.getElementById('date_fin');
+
+    const valueDateDeb = dateDebAppointment.value;
+
+    if(valueDateDeb){
+        dateFinAppointment.setAttribute('min', valueDateDeb);
+    }else {
+        dateFinAppointment.removeAttribute('min');
+    }
+    validiteDateFin();
+}
+
+// Permet de ne pas avoir d'heure de fin antérieur à l'heure de début (sur le même jour)
+function updateHeureFin() {
+    // Même jour
+    let dateDebAppointment = document.getElementById('date_debut');
+    let dateFinAppointment = document.getElementById('date_fin');
+    let heureDebut = document.getElementById('heure_debut');
+    let heureFin = document.getElementById('heure_fin');
+    
+    if (dateDebAppointment.value === dateFinAppointment.value) {
+        if (heureDebut.value > heureFin.value)
+            heureFin.value = heureDebut.value;
+    }
+}
 
 function afficherCalendar(){
     const selectionSpecialDate = document.getElementById('fin_rec');
