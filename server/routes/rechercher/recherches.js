fd1e27fc8c3f8c9c04e03ace46a0db32f87d0c3a
houssein <saban.houssein1@gmail.com>
2025-11-28 14:06:50 +0100

    ajout de l'affichage des rdv récurent dans la barre de recherche

--- server/routes/rechercher/recherches.js
+++ server/routes/rechercher/recherches.js
@@ -25,19 +25,66 @@ routeRecherche.post("/recherche", async (req, res, next) => {
 
 /**
  * Fonction qui retourne les rendez-vous correspondant à l'entrée utilisateur
- * @param {entrée saisie par l'utilisateur dans la barre de recherche} str 
+ * @param {entrée saisie par l'utilisateur dans la barre de recherche} str
+ * @param {Id des agendas dans laquel la recherche est faite} agendaIds
+ * @param {*} next
+ * @returns une liste de rdv (à afficher).
  */
 async function rechercheRendezVous(str, agendaIds, next) {
   try {
-    if(!str) {
-      return "";
-    } else {
-      const appointments = await AppointmentModel.find({
-          agenda: { $in: agendaIds },
-          nom: { $regex: new RegExp(`^${str}`, 'i') }
-      })
-      return appointments;
+    if (!str)
+      return []; 
+
+    const appointments = await AppointmentModel.find({
+        agenda: { $in: agendaIds },
+        nom: { $regex: new RegExp(`^${str}`, 'i') }
+    }).populate('recurrenceRule'); 
+
+    const resultatsFinaux = [];
+    const dateMax = new Date();
+    dateMax.setFullYear(dateMax.getFullYear() + 1);
+
+    for(let rdv of appointments) {
+      let rdvOriginal = rdv.toObject();
+      // Si pas de récurrence
+      if(!rdv.recurrenceRule) {
+          resultatsFinaux.push(rdvOriginal);
+          continue; 
+      }
+      // Si récurrence
+      const regle = rdv.recurrenceRule;
+      const dureeMs = new Date(rdv.date_Fin).getTime() - new Date(rdv.date_Debut).getTime();
+      
+      let dateDebutReccurence = new Date(rdv.date_Debut);
+      let dateFinRecurrence = regle.date_fin ? new Date(regle.date_fin) : dateMax;
+      let intervalle = parseInt(regle.intervale);
+      if (isNaN(intervalle) || intervalle < 1) intervalle = 1;
+      while(dateDebutReccurence <= dateFinRecurrence) {
+        resultatsFinaux.push({
+            ...rdvOriginal, 
+            date_Debut: new Date(dateDebutReccurence), 
+            date_Fin: new Date(dateDebutReccurence.getTime() + dureeMs)
+        });
+        switch(regle.frequence) {
+          case 'day1':
+            dateDebutReccurence.setDate(dateDebutReccurence.getDate() + intervalle);
+            break;
+          case 'week1':
+            dateDebutReccurence.setDate(dateDebutReccurence.getDate() + (7 * intervalle));
+            break;
+          case 'month1':
+            dateDebutReccurence.setMonth(dateDebutReccurence.getMonth() + intervalle);
+            break;
+          case 'year1':
+            dateDebutReccurence.setFullYear(dateDebutReccurence.getFullYear() + intervalle);
+            break;
+          default:
+            break;
+        }
+      }
     }
+    resultatsFinaux.sort((a, b) => a.date_Debut - b.date_Debut);
+    return resultatsFinaux;
   } catch (error) {
     next(error);
   } 
